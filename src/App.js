56ba957008bbfa429ab926ed63b11c38867fc2ee
GarimaRajput18 <garima.rajput@krazy-tech.com>
2024-07-06 23:48:23 +0530

    Basic components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
-     <Navbar/>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path = '/' element={<Shop/>}/>
+        <Route path='/men' element={<ShopCategory category = "men"/>}/>
+        <Route path='/women' element={<ShopCategory category = "women"/>}/>
+        <Route path='/kids' element={<ShopCategory category = "kids"/>}/>
+        <Route path='/product' element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path = '/cart' element={<Cart/>}/>
+        <Route path = '/login' element={<LoginSignup/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
+     
     </div>
   );
 }
---
+++ src/Pages/ShopCategory.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const ShopCategory = () => {
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default ShopCategory 
