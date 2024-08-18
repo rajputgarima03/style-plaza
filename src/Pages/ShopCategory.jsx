567e1f04b3487f9a7550f8c4f49408c9817ce213
GarimaRajput18 <garima.rajput@krazy-tech.com>
2024-08-18 22:13:00 +0530

    added the product category api also

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,12 +1,41 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../Context/ShopContext';
 import './CSS/ShopCategory.css';
 import dropdown_icon from '../Components/Assets/dropdown_icon.png'
 import Item from '../Components/Item/Item';
+import { envUrl } from '../Utils/env';
+import config from '../Utils/config.json';
 
 
 const ShopCategory = (props) => {
-    const {allProducts} = useContext(ShopContext)
+    const {allProducts} = useContext(ShopContext);
+    const [categoryData, setCategoryData] = useState([]);
+    const[error, setError] = useState(null);
+
+    useEffect(()=>{
+     getCategoryData();
+    },[props])
+
+   const getCategoryData = async() =>{
+    try{ 
+        const response = await fetch (`${envUrl()}${config['category']}/${props.category}`);
+        const data = await response.json();
+        if(data){
+            setCategoryData(data); 
+            setError(false)
+        }
+        else{
+            setError(true)
+            setCategoryData([])
+        }
+       
+    }
+catch(error){
+setError(true)
+
+}
+
+    }
     return (
         <div className='shop-category'>
            <img className='shopcategory-banner' src={props.banner} alt =""/>
@@ -19,14 +48,16 @@ const ShopCategory = (props) => {
             </div> */}
            </div>
            <div className='shopcategory-products'>
-              {allProducts?.map((item,i)=>{
-               if(props.category === item.category){
+              {categoryData.length >0 && categoryData?.map((item,i)=>{
                 return <Item key={i} id={item.productId} name={item.productName} new_price={item.productNewPrice} old_price={item.productOldPrice} image={item.productImage} />
-               }
-               else{
-                return null
-               }
               })}
+              {
+                categoryData.length == 0 &&  (
+                    <div className="no-data-block">
+                        <p>No data to show</p>
+                    </div>
+                )
+              }
            </div>
            {/* <div className='shopcategory-loadmore'>
              Explore More
