78d06323e9f25455b799a61da6aa0df94c9009ea
Rahul Rajput <rahul.rajput@paidy.com>
2024-08-18 18:08:20 +0530

    login pages fixes

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import './CSS/Loginsignup.css'
+import React, { useState } from "react";
+import "./CSS/Loginsignup.css";
 
 const LoginSignup = () => {
-    return (
-        <div className='loginsignup'>
-           <div className='loginsignup-container'>
-            <h1>Sign Up</h1>
-           <div className='loginsignup-fields'>
-             <input type="text" placeholder='Your Name'/>
-             <input type="email" placeholder='Your Email'/>
-             <input type="password" placeholder='Your Pasword'/>
-        </div>
-        <button>Continue</button>
-        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
-        <div className='loginsignup-agree'>
-            <input type="checkbox" name="" id=""/>
-            <p>By continuing, I agree to use the terms and privacy policy</p>
-        </div>
-         </div>
-        </div>
-    )
-}
+  const [pageType, setPageType] = useState("signup"); // default value signup
 
-export default LoginSignup 
+  const handleLogin = () => {
+    setPageType("login");
+  };
+
+  const handleSignup = () => {
+    setPageType("signup");
+  };
+
+  if (pageType === "signup") {
+    return (
+      <div className="loginsignup">
+        <div className="loginsignup-container">
+          <h1>Sign Up</h1>
+          <div className="loginsignup-fields">
+            <input type="text" placeholder="Your Name" />
+            <input type="email" placeholder="Your Email" />
+            <input type="password" placeholder="Your Password" />
+          </div>
+          <button>Continue</button>
+          <p className="loginsignup-login">
+            Already have an account?{" "}
+            <span onClick={handleLogin}>Login here</span>
+          </p>
+          <div className="loginsignup-agree">
+            <input type="checkbox" name="" id="" />
+            <p>By continuing, I agree to use the terms and privacy policy</p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="loginsignup">
+        <div className="loginsignup-container">
+          <h1>Login</h1>
+          <div className="loginsignup-fields">
+            <input type="email" placeholder="Your Email" />
+            <input type="password" placeholder="Your Password" />
+          </div>
+          <button>Continue</button>
+          <p className="loginsignup-login">
+            Don't have an account?{" "}
+            <span onClick={handleSignup}>Signup here</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default LoginSignup;
